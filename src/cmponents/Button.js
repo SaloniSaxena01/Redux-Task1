b643a3a52d1475526c6baa99ea2c578071e9dae2
SaloniSaxena01 <60663789+SaloniSaxena01@users.noreply.github.com>
2021-05-27 20:27:10 +0530

    Add files via upload

---
+++ src/cmponents/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Button} from "react-bootstrap";
+import {increment} from '../redux/actions/increment'
+import {useDispatch} from 'react-redux'
+
+const Click = () => {
+    const dispatch = useDispatch();
+    return(
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height:'100vh'
+        }}>
+            <Button variant={"primary"} onClick={() => dispatch(increment())}>Click Me</Button>
+        </div>
+    )
+}
+
+export default Click;
